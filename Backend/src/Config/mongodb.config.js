58f9ff76033ab14711dd1a4b31da4de076659bde
Sergei Mankov <sergei13@hotmail.de>
2025-01-02 15:13:57 +0100

    Deleted whitespaces

--- Backend/src/Config/mongodb.config.js
+++ Backend/src/Config/mongodb.config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectMongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB connected")
+        console.log("MongoDB connected");
     } catch (error){
         throw error;
     }
